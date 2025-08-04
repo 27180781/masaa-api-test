41f375fca73cca3ad89f9a5882f94e6de138fed1
Nissim <NA73438@gmail.com>
2025-08-04 06:28:36 +0300

    שינוי יצירת תמונות

--- image-generator/config.js
+++ image-generator/config.js
@@ -22,7 +22,7 @@ const FONTS = {
     groupTitle: 'FbKanuba Bold 48',
     noDataMessage: 'FbKanuba Bold 60',
     participantName: 'FbKanuba Bold 32',
-    legendText: 'FbKanuba 36'
+    legendText: 'FbKanuba 38'
 };
 
 const LAYOUT = {
