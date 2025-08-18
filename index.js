7e6517d72fa8868a31cb5d2a5347048379c8cfe8
Nissim <NA73438@gmail.com>
2025-08-19 00:02:58 +0300

    שינוי גודל קובץ מתקבל

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ const MAX_LOG_HISTORY = 50;
 //                             MIDDLEWARE
 // ===================================================================
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// הוספת מגבלה של 10 מגה-בייט
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // =================================================================
 //                      PUBLIC & ADMIN ROUTES
 // ===================================================================
